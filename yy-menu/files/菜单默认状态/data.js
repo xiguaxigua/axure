38a20f5c5c9636242c2c22a3e7274eddfa5f8601
Melon <guang.xing@liulishuo.com>
2019-11-06 20:35:16 +0800

    update

---
+++ yy-menu/files/菜单默认状态/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(p,q),r,[s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,N,N,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(H,I,J,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,J,_(bk,bl,bm,bl,bn,bl,bo,bp)),i,_(j,k,l,bq)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="菜单默认状态.html",d="generationDate",e=new Date(1573043658847.46),f="defaultAdaptiveView",g="name",h="基本",i="size",j="width",k=1024,l="height",m=768,n="adaptiveViews",o="sketchKeys",p="",q="s0",r="variables",s="OnLoadVariable",t="page",u="packageId",v="f6c0fb5c1d0d4ecd8db31ce967513b2b",w="type",x="Axure:Page",y="菜单默认状态",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="center",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="near",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="r",bl=0,bm="g",bn="b",bo="a",bp=0.349019607843137,bq=0,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
---
+++ yy-menu/files/鼠标浮在侧边栏/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(p,q),r,[s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,N,N,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(H,I,J,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,J,_(bk,bl,bm,bl,bn,bl,bo,bp)),i,_(j,k,l,bq)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="鼠标浮在侧边栏.html",d="generationDate",e=new Date(1573043658877.01),f="defaultAdaptiveView",g="name",h="基本",i="size",j="width",k=1024,l="height",m=768,n="adaptiveViews",o="sketchKeys",p="",q="s0",r="variables",s="OnLoadVariable",t="page",u="packageId",v="7899552d19cc426f9f129ad88d13bd05",w="type",x="Axure:Page",y="鼠标浮在侧边栏",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="center",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="near",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="r",bl=0,bm="g",bn="b",bo="a",bp=0.349019607843137,bq=0,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
